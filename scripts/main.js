e8aeb35e0ab8969339330e3e406155edbb9acb21
ctYurk15 <levgenetic@gmail.com>
2021-03-18 17:35:50 +0200

    Added ability to leave account, blank pages and page for credits

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,10 @@ var header = "<td width='14.28%'>"+
         "<a href='sales.html'>Акції</a>"+
     "</td>"+
     "<td width='14.28%'>"+
-        "<a href='#'>Кредитування</a>"+
+        "<a href='crediting.html'>Кредитування</a>"+
     "</td>"+
     "<td width='14.28%'>"+
-        "<a href='#'>Про нас</a>"+
+        "<a href='about.html'>Про нас</a>"+
     "</td>"+
     "<td width='14.28%'>"+
         "<img src='images/account.png' width='35%' id='accountImg'>"+
@@ -24,22 +24,22 @@ var footer = "<td width='14.28%''>"+
                     "<img src='images/logo.png' width='35%'>"+
                 "</td>"+
                 "<td width='14.28%'>"+
-                    "<a href='#'>Контакти</a>"+
+                    "<a href='footerPage.html'>Контакти</a>"+
                 "</td>"+
                 "<td width='28.56%' colspan='2'>"+
-                    "<a href='#'>Політика конфіденційності</a>"+
+                    "<a href='footerPage.html'>Політика конфіденційності</a>"+
                 "</td>"+
                 "<td width='28.56%' colspan='2'>"+
-                    "<a href='#'>Юридична інформація</a>"+
+                    "<a href='footerPage.html'>Юридична інформація</a>"+
                 "</td>"+
                 "<td width='14.28%'>"+
-                    "<a href='#'>Про нас</a>"+
+                    "<a href='footerPage.html'>Про нас</a>"+
                 "</td>";
 
 var accountDiv = "<div id='accountDiv'>"+
                         "<button>Аккаунт</button>"+
                         "<button>Вантажівка</button>"+
-                        "<button>Вийти</button>"+
+                        "<button id='accountLeave'>Вийти</button>"+
                     "</div>";
 
 //adding header and footer
@@ -47,6 +47,7 @@ document.getElementById("headerTR").innerHTML += header;
 document.getElementById("footerTR").innerHTML += footer;
 
 $(document).ready(function(){
+    //showing account div
    $("#accountImg").on("click", function(){
        $("#accountDiv").toggleClass("slideHide");
    });
@@ -61,4 +62,30 @@ $(document).ready(function(){
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
         return parts.join(".");
     }
+    
+    //if we pressed on leave account button
+    $("#accountLeave").on("click", function(){
+        $("#dialogDiv").removeClass("hidden");
+        $("#transparentDiv").removeClass("hidden");
+    });
+    
+    //if we canceled it
+    $("#no").on("click", function(){
+        $("#dialogDiv").addClass("hidden");
+        $("#transparentDiv").addClass("hidden");
+    });
+    
+    $(document).on("click", function(event){
+        if(event.target.id != "accountImg")
+        {
+            if( event.target.localName != "button")
+            {
+                $("#dialogDiv").addClass("hidden");
+                $("#transparentDiv").addClass("hidden");
+                //$("#accountDiv").addClass("slideHide");
+            }
+            //console.log(event.target.id);
+        }
+        //else $("#transparentDiv").toggleClass("hidden");
+    });
 });
